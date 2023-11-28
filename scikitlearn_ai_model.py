a18c8b092da4115f2cae5121925948990ceacf13
Santosh Kulkarni <santoshkulkarni@Santoshs-MacBook-Pro.local>
2023-11-28 08:32:06 -0500

    scikit learn model

---
+++ scikitlearn_ai_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import Image as PImage
+from subprocess import check_call
+from PIL import Image, ImageDraw, ImageFont
+from sklearn.tree import export_graphviz
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+
+turnover = pd.read_csv("turnover.csv")
+turnover.head()
+
+# converting salary into categories
+turnover["salary"] = turnover["salary"].astype('category').cat.reorder_categories(['low', 'medium', 'high']).cat.codes
+turnover.head()
